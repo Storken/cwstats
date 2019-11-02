07ce44d02948350bdb7c4a7020a77e9e810e440f
Storken <magnushuttu@gmail.com>
2019-11-02 18:49:09 +0100

    Update to portfolio site

---
+++ precache-manifest.cbf208cb55063a80e8b66d24acfa8327.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b163753cc15d3ca52d2420bce63eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a429415fd8c69eb922e",
+    "url": "/static/css/main.f1786e9f.chunk.css"
+  },
+  {
+    "revision": "840a2f99eeff5b0f6e0e",
+    "url": "/static/js/2.03f7b712.chunk.js"
+  },
+  {
+    "revision": "8a429415fd8c69eb922e",
+    "url": "/static/js/main.a4068e92.chunk.js"
+  },
+  {
+    "revision": "578d431d20714ddbbb1d",
+    "url": "/static/js/runtime-main.552b7e17.js"
+  },
+  {
+    "revision": "6cc74a74da44c7aef23e377e031333c3",
+    "url": "/static/media/catatonicclub.6cc74a74.png"
+  },
+  {
+    "revision": "5f7a1909cdc1c51f8c4bfc199da8d5d2",
+    "url": "/static/media/cloud.5f7a1909.svg"
+  },
+  {
+    "revision": "97f128c725b78a0477dfbcad6e84ce10",
+    "url": "/static/media/cwstats.97f128c7.png"
+  },
+  {
+    "revision": "fc66c076e950f71c50d1427fe585b873",
+    "url": "/static/media/heavencat.fc66c076.png"
+  },
+  {
+    "revision": "c8e43b1e08cd4a1eeff3d42e4ca1441d",
+    "url": "/static/media/magnus.c8e43b1e.png"
+  },
+  {
+    "revision": "4c3a6ec3c29dc888b899420deb2637db",
+    "url": "/static/media/myaccountvasttrafik.4c3a6ec3.png"
+  },
+  {
+    "revision": "742a6cd9d154d643e173440c92f53a51",
+    "url": "/static/media/sun-sky.742a6cd9.svg"
+  },
+  {
+    "revision": "d0fe7a660fe7d1df17b0217d7609abf0",
+    "url": "/static/media/vasttrafik.d0fe7a66.png"
+  },
+  {
+    "revision": "4be783986f8f6a22f927296a64f20e4a",
+    "url": "/static/media/white-arrow.4be78398.svg"
+  }
+]);
